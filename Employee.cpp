0ebeaeeea9025f5ce2e8855cb034c5499a6b11c0
ra068632 <ra068632@cerner.com>
2020-11-10 23:21:51 -0600

    Add project files.

---
+++ Employee.cpp
@@ -0,0 +1,9 @@
+// Your name
+// Your UMKC Email Address
+// Today's date
+// CS201L Lab 12
+
+#include <string>
+#include "Employee.h"
+using namespace std;
+
---
+++ Lab12.cpp
@@ -0,0 +1,4 @@
+// Your name
+// Your UMKC Email Address
+// Today's date
+// CS201L Lab 12
